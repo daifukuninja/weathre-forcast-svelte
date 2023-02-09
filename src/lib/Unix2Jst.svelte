f004cf621c16490aa9b5ed23d87f401e743dc9cc
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-02-09 15:50:19 +0000

    add: unixtimeを年月日に変換するコンポーネントの追加

---
+++ src/lib/Unix2Jst.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let unixtime = 1675954800;
+    let dt = new Date(unixtime * 1000);
+    let y = dt.getFullYear();
+    let m = dt.getMonth()+1;
+    let d = dt.getDate();
+    let h = dt.getHours().toString().padStart( 2, '0');
+    let mm = dt.getMinutes().toString().padStart( 2, '0');
+    let s = dt.getSeconds().toString().padStart( 2, '0');
+    let jst = `${y}年${m}月${d}日 ${h}:${mm}:${s}`
+</script>
+
+<span>{jst}</span>
