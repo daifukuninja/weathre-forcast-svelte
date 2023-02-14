794d3f5e1d51760aa267804ccfbeaee82724a02f
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-02-14 23:38:27 +0000

    add: degreesから風向表記を作るコンポーネントの実装

---
+++ src/lib/Direction.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { wind_degrees2label } from "../scripts/wind_degrees2label";
+    export let deg: number;
+
+    let direction: string = wind_degrees2label(deg);
+</script>
+
+<span>{direction}</span>
