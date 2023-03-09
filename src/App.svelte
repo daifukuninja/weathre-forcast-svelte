12228dcb53d0082d8960bf5a068d6037f1012a38
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-09 07:37:38 +0000

    update: 天気予報カードの組み込み

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
+  import ForcastCard from "./lib/ForcastCard.svelte";
   import WeatherCard from "./lib/WeatherCard.svelte";
 
   let place: string = "hirakue";
 
   const handleClick = () => {
-    const elem = (document.activeElement as HTMLElement);
-    if(elem){
+    const elem = document.activeElement as HTMLElement;
+    if (elem) {
       elem?.blur();
     }
-  }
+  };
 </script>
 
 <div class="container px-0">
@@ -29,17 +30,24 @@
         </button>
         <ul class="dropdown-content menu p-2 bg-base-100 rounded-box w-52">
           <li>
-            <button on:click={() => (place = "hirakue")} on:click={handleClick}>茨城県水戸市</button>
+            <button on:click={() => (place = "hirakue")} on:click={handleClick}
+              >茨城県水戸市</button
+            >
           </li>
           <li>
-            <button on:click={() => (place = "tanbara")} on:click={handleClick}>群馬県沼田市</button>
+            <button on:click={() => (place = "tanbara")} on:click={handleClick}
+              >群馬県沼田市</button
+            >
           </li>
           <li>
-            <button on:click={() => (place = "sapporo")} on:click={handleClick}>北海道札幌市</button>
+            <button on:click={() => (place = "sapporo")} on:click={handleClick}
+              >北海道札幌市</button
+            >
           </li>
           <li>
-            <button on:click={() => (place = "hitachinaka")} on:click={handleClick}
-              >茨城県ひたちなか市</button
+            <button
+              on:click={() => (place = "hitachinaka")}
+              on:click={handleClick}>茨城県ひたちなか市</button
             >
           </li>
         </ul>
@@ -49,42 +57,7 @@
   <main class="p-4">
     {#key place}
       <WeatherCard {place} />
+      <ForcastCard {place} />
     {/key}
-
-    <p class="mb-6">
-      What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
-      typesetting industry. Lorem Ipsum has been the industry's standard dummy
-      text ever since the 1500s, when an unknown printer took a galley of type
-      and scrambled it to make a type specimen book. It has survived not only
-      five centuries, but also the leap into electronic typesetting, remaining
-      essentially unchanged. It was popularised in the 1960s with the release of
-      Letraset sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of
-      Lorem Ipsum.
-    </p>
-
-    <p class="mb-6">
-      What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
-      typesetting industry. Lorem Ipsum has been the industry's standard dummy
-      text ever since the 1500s, when an unknown printer took a galley of type
-      and scrambled it to make a type specimen book. It has survived not only
-      five centuries, but also the leap into electronic typesetting, remaining
-      essentially unchanged. It was popularised in the 1960s with the release of
-      Letraset sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of
-      Lorem Ipsum.
-    </p>
-
-    <p class="mb-6">
-      What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
-      typesetting industry. Lorem Ipsum has been the industry's standard dummy
-      text ever since the 1500s, when an unknown printer took a galley of type
-      and scrambled it to make a type specimen book. It has survived not only
-      five centuries, but also the leap into electronic typesetting, remaining
-      essentially unchanged. It was popularised in the 1960s with the release of
-      Letraset sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of
-      Lorem Ipsum.
-    </p>
   </main>
 </div>
