c2c92faf64c0d5c5b83a494f7157eb28b8be1ed0
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-10 13:14:05 +0000

    fix: menuの改善テスト(再)

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,12 @@
       elem?.blur();
     }
   };
+
+  const showDropdownOptions = () => {
+        document.getElementById("options").classList.toggle("hidden");
+        document.getElementById("arrow-up").classList.toggle("hidden");
+        document.getElementById("arrow-down").classList.toggle("hidden");
+    }
 </script>
 
 <div class="container px-0">
@@ -20,30 +26,21 @@
       >
     </div>
 
-    <div class=" relative inline-block text-left dropdown">
-      <span class="rounded-md shadow-sm"
-        ><button class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800" 
-         type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
-          <span>Options</span>
-          <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-          </button
-      ></span>
-      <div class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
-        <div class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
-          <div class="px-4 py-3">         
-            <p class="text-sm leading-5">Signed in as</p>
-            <p class="text-sm font-medium leading-5 text-gray-900 truncate">tom@example.com</p>
+    <div class="flex flex-row justify-center bg-gray-100">
+      <div class="flex-none p-2">
+          <button on:click={showDropdownOptions} class="flex flex-row justify-between w-48 px-2 py-2 text-gray-700 bg-white border-2 border-white rounded-md shadow focus:outline-none focus:border-blue-600">
+              <span class="select-none">Select an item</span>
+  
+              <svg id="arrow-down" class="hidden w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
+              <svg id="arrow-up" class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" /></svg>
+          </button>
+          <div id="options" class="hidden w-48 py-2 mt-2 bg-white rounded-lg shadow-xl">
+              <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Item 1</a>
+              <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Item 2</a>
+              <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Item 3</a>
           </div>
-          <div class="py-1">
-            <a href="javascript:void(0)" tabindex="0" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Account settings</a>
-            <a href="javascript:void(0)" tabindex="1" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Support</a>
-            <span role="menuitem" tabindex="-1" class="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 cursor-not-allowed opacity-50" aria-disabled="true">New feature (soon)</span>
-            <a href="javascript:void(0)" tabindex="2" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left" role="menuitem" >License</a></div>
-          <div class="py-1">
-            <a href="javascript:void(0)" tabindex="3" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Sign out</a></div>
-        </div>
       </div>
-    </div>
+  </div>
 
         <!-- <div class="flex-none">
       <div class="dropdown dropdown-end px-1 text-secondary-content">
