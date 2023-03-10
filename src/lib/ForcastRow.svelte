aee2abbf7cd83b45f1909080741b24ab555d6024
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-10 15:17:17 +0000

    update: アイコンと日時を表示するようにカードを改善

--- src/lib/ForcastRow.svelte
+++ src/lib/ForcastRow.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
     import type { ForcastRow } from "../scripts/interface_forcast";
+    import K2c from "./K2c.svelte";
+    import WeatherIcon from "./WeatherIcon.svelte";
+    import Unix2JstHhMm from "./Unix2JstHhMm.svelte";
     export let row: ForcastRow;
 </script>
 
 <div>
-    <div>{row.main.temp}</div>
-    <div>{row.weather[0].description}</div>
+    <div><WeatherIcon icon_id={row.weather[0].icon} /></div>
+    <div><Unix2JstHhMm unixtime={row.dt} /></div>
+    <div class="temp"><K2c kelvin={row.main.temp} /></div>
 </div>
+
+<style>
+    .temp {
+        font-size: small;
+    }
+</style>
