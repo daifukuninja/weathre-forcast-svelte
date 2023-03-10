7fe734185665e6429ccbc88952790aedc6913e31
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-10 13:36:51 +0000

    fix: dropdownをsvelteコンポーネントで対応

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import ForcastCard from "./lib/ForcastCard.svelte";
   import WeatherCard from "./lib/WeatherCard.svelte";
+  import Dropdown from "./lib/Dropdown.svelte";
 
   let place: string = "hirakue";
 
@@ -11,11 +12,6 @@
     }
   };
 
-  const showDropdownOptions = () => {
-        document.getElementById("options").classList.toggle("hidden");
-        document.getElementById("arrow-up").classList.toggle("hidden");
-        document.getElementById("arrow-down").classList.toggle("hidden");
-    }
 </script>
 
 <div class="container px-0">
@@ -26,23 +22,7 @@
       >
     </div>
 
-    <div class="flex flex-row justify-center bg-gray-100">
-      <div class="flex-none p-2">
-          <button on:click={showDropdownOptions} class="flex flex-row justify-between w-48 px-2 py-2 text-gray-700 bg-white border-2 border-white rounded-md shadow focus:outline-none focus:border-blue-600">
-              <span class="select-none">Select an item</span>
-  
-              <svg id="arrow-down" class="hidden w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
-              <svg id="arrow-up" class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" /></svg>
-          </button>
-          <div id="options" class="hidden w-48 py-2 mt-2 bg-white rounded-lg shadow-xl">
-              <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Item 1</a>
-              <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Item 2</a>
-              <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Item 3</a>
-          </div>
-      </div>
-  </div>
-
-        <!-- <div class="flex-none">
+    <!-- <div class="flex-none">
       <div class="dropdown dropdown-end px-1 text-secondary-content">
         <button class="m-1 w-20">
           <svg class="fill-current" width="20" height="20" viewBox="0 0 24 24"
@@ -76,6 +56,7 @@
         </ul>
       </div>
     </div> -->
+    <Dropdown />
   </header>
   <main class="p-4">
     {#key place}
@@ -85,10 +66,3 @@
   </main>
 </div>
 
-<style>
-  .dropdown:focus-within .dropdown-menu {
-    opacity:1;
-    transform: translate(0) scale(1);
-    visibility: visible;
-  }
-      </style>
