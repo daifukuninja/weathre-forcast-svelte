5e5c64fd73cd2d2be176884d89d0d3b895d9bb25
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-04-14 16:31:00 +0000

    add: フッダーのテスト実装

--- src/App.svelte
+++ src/App.svelte
@@ -7,8 +7,8 @@
 
 </script>
 
-<div class="container px-0">
-  <header class="sticky top-0 navbar bg-secondary shadow p-4 z-50">
+<div class="px-0">
+  <header class="sticky top-0 navbar bg-secondary shadow p-4 z-50 md:flex md:items-center md:justify-between md:p-6 ">
     <div class="flex-1">
       <button class="btn btn-ghost normal-case text-xl text-secondary-content"
         >スーパー地元天気予報</button
@@ -22,5 +22,23 @@
       <ForcastCard {place} />
     {/key}
   </main>
+  <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
+        class="hover:underline">Contribe Co.,Ltd</a> All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+      <li>
+        <a href="{null}" class="mr-4 hover:underline md:mr-6 ">About</a>
+      </li>
+      <li>
+        <a href="{null}" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+      </li>
+      <li>
+        <a href="{null}" class="mr-4 hover:underline md:mr-6">Licensing</a>
+      </li>
+      <li>
+        <a href="{null}" class="hover:underline">Contact</a>
+      </li>
+    </ul>
+  </footer>  
 </div>
-
